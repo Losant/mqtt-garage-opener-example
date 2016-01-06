83773dc45ece4bcaba2cbd4b3802abdc92927e88
Gergely Nemeth <mail@nemethgergely.com>
2016-01-06 08:39:10 +0100

    fix(style): minor style fixes

--- controller.js
+++ controller.js
@@ -1,63 +1,61 @@
 // controller.js
-var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://broker.hivemq.com');
+const mqtt = require('mqtt')
+const client = mqtt.connect('mqtt://broker.hivemq.com')
 
-var garageState = '';
-var connected = false;
+var garageState = ''
+var connected = false
 
-client.on('connect', function() {
-  client.subscribe('garage/connected');
-  client.subscribe('garage/state');
-});
+client.on('connect', () => {
+  client.subscribe('garage/connected')
+  client.subscribe('garage/state')
+})
 
-client.on('message', function(topic, message) {
-  switch(topic) {
+client.on('message', (topic, message) => {
+  switch (topic) {
     case 'garage/connected':
-      return handleGarageConnected(message);
-      break;
+      return handleGarageConnected(message)
     case 'garage/state':
-      return handleGarageState(message);
-      break;
+      return handleGarageState(message)
   }
-  console.log('No handler for topic %s', topic);
-});
+  console.log('No handler for topic %s', topic)
+})
 
-function handleGarageConnected(message) {
-  console.log('garage connected status %s', message);
-  connected = (message.toString() === 'true');
+function handleGarageConnected (message) {
+  console.log('garage connected status %s', message)
+  connected = (message.toString() === 'true')
 }
 
-function handleGarageState(message) {
-  garageState = message;
-  console.log('garage state update to %s', message);
+function handleGarageState (message) {
+  garageState = message
+  console.log('garage state update to %s', message)
 }
 
-function openGarageDoor() {
+function openGarageDoor () {
   // can only open door if we're connected to mqtt and door isn't already open
-  if(connected && garageState !== 'open') {
+  if (connected && garageState !== 'open') {
     // Ask the door to open
-    client.publish('garage/open', 'true');
+    client.publish('garage/open', 'true')
   }
 }
 
-function closeGarageDoor() {
+function closeGarageDoor () {
   // can only close door if we're connected to mqtt and door isn't already closed
-  if(connected && garageState !== 'closed') {
+  if (connected && garageState !== 'closed') {
     // Ask the door to close
-    client.publish('garage/close', 'true');
+    client.publish('garage/close', 'true')
   }
 }
 
-//--- For Demo Purposes Only ----//
+// --- For Demo Purposes Only ----//
 
 // simulate opening garage door
-setTimeout(function() {
-  console.log('open door');
-  openGarageDoor();
-}, 5000);
+setTimeout(() => {
+  console.log('open door')
+  openGarageDoor()
+}, 5000)
 
 // simulate closing garage door
-setTimeout(function() {
-  console.log('close door');
-  closeGarageDoor();
-}, 20000);
+setTimeout(() => {
+  console.log('close door')
+  closeGarageDoor()
+}, 20000)
--- garage.js
+++ garage.js
@@ -1,86 +1,90 @@
 // garage.js
-var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://broker.hivemq.com');
+const mqtt = require('mqtt')
+const client = mqtt.connect('mqtt://broker.hivemq.com')
 
 /**
  * The state of the garage, defaults to closed
  * Possible states : closed, opening, open, closing
  */
-var state = 'closed';
+var state = 'closed'
 
-client.on('connect', function () {
-  client.subscribe('garage/open');
-  client.subscribe('garage/close');
+client.on('connect', () => {
+  client.subscribe('garage/open')
+  client.subscribe('garage/close')
 
   // Inform controllers that garage is connected
-  client.publish('garage/connected', 'true');
-  sendStateUpdate();
-});
+  client.publish('garage/connected', 'true')
+  sendStateUpdate()
+})
 
-client.on('message', function(topic, message) {
-  console.log('received message %s %s', topic, message);
+client.on('message', (topic, message) => {
+  console.log('received message %s %s', topic, message)
   switch (topic) {
     case 'garage/open':
-      return handleOpenRequest(message);
-      break;
+      return handleOpenRequest(message)
     case 'garage/close':
-      return handleCloseRequest(message);
-      break;
+      return handleCloseRequest(message)
   }
-});
+})
 
-function sendStateUpdate() {
-  console.log('sending state %s', state);
-  client.publish('garage/state', state);
+function sendStateUpdate () {
+  console.log('sending state %s', state)
+  client.publish('garage/state', state)
 }
 
-function handleOpenRequest(message) {
-  if(state !== 'open' && state !== 'opening') {
-    console.log('opening garage door');
-    state = 'opening';
-    sendStateUpdate();
+function handleOpenRequest (message) {
+  if (state !== 'open' && state !== 'opening') {
+    console.log('opening garage door')
+    state = 'opening'
+    sendStateUpdate()
 
     // simulate door open after 5 seconds (would be listening to hardware)
-    setTimeout(function() {
-      state = 'open';
-      sendStateUpdate();
-    }, 5000);
+    setTimeout(() => {
+      state = 'open'
+      sendStateUpdate()
+    }, 5000)
   }
 }
 
-function handleCloseRequest(message) {
-  if(state !== 'closed' && state !== 'closing') {
-    state = 'closing';
-    sendStateUpdate();
+function handleCloseRequest (message) {
+  if (state !== 'closed' && state !== 'closing') {
+    state = 'closing'
+    sendStateUpdate()
 
     // simulate door closed after 5 seconds (would be listening to hardware)
-    setTimeout(function() {
-      state = 'closed';
-      sendStateUpdate();
-    }, 5000);
+    setTimeout(() => {
+      state = 'closed'
+      sendStateUpdate()
+    }, 5000)
   }
 }
 
 /**
  * Want to notify controller that garage is disconnected before shutting down
  */
-function handleAppExit(options, err) {
-  if(err) {
-    console.log(err.stack);
+function handleAppExit (options, err) {
+  if (err) {
+    console.log(err.stack)
   }
 
-  if(options.cleanup) {
-    client.publish('garage/connected', 'false');
+  if (options.cleanup) {
+    client.publish('garage/connected', 'false')
   }
 
-  if(options.exit) {
-    process.exit();
+  if (options.exit) {
+    process.exit()
   }
 }
 
 /**
  * Handle the different ways an application can shutdown
  */
-process.on('exit', handleAppExit.bind(null, { cleanup : true }));
-process.on('SIGINT', handleAppExit.bind(null, { exit : true }));
-process.on('uncaughtException', handleAppExit.bind(null, { exit : true }));
+process.on('exit', handleAppExit.bind(null, {
+  cleanup: true
+}))
+process.on('SIGINT', handleAppExit.bind(null, {
+  exit: true
+}))
+process.on('uncaughtException', handleAppExit.bind(null, {
+  exit: true
+}))
